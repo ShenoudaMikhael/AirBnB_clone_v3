e183cee70564f07ea577f6998e9f5abd8b81da7b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-25 21:50:26 +0300

    7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,37 +3,16 @@
 
 from api.v1.views import app_views
 from models import storage
-from models.city import City
+from models.state import State
 from flask import request, abort
 
 
 @app_views.route("/states/<state_id>/cities")
 def get_cities(state_id):
-    """get all cities"""
+    """get all cities of state"""
+    # Start hereee  Bellaaaaaaaaaaaa
     if state_id:
-        if storage.get(City, id):
-            return storage.get(City, id).to_dict()
+        if storage.get(State, id):
+            return storage.get(State, id).cities
         abort(404)
-    return [s.to_dict() for s in storage.all(City).values()]
-
-
-@app_views.route("/cities", methods=["post"])
-def post_city():
-    """post city"""
-    data = request.get_json()
-    a = City(**data)
-    storage.new(a)
-    storage.save()
-    return a.to_dict(), 201
-
-
-@app_views.route("/cities/<id>", methods=["delete"])
-def delete_city(id):
-    """delete all cities"""
-    if id:
-        st = storage.get(City, id)
-        if st:
-            storage.delete(st)
-            storage.save()
-            return {}, 200
-        abort(404)
+    return {}
