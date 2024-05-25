3b6451e9fd320fe9db974c503aa18f4754e01c65
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-25 16:49:52 +0300

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,18 @@
 """create a file index_.py"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status")
 def get_status():
     """get status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def get_stats():
+    """get stats"""
+    # add storage.count after merge
+    return jsonify({})
+    # return jsonify(storage.count)
