19cb60ebd0bbb861056af9b1378d06f2f828914f
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-25 16:52:35 +0300

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,19 @@
 starts a Flask web application
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """Not Found Handler"""
+    return jsonify({"error": "Not found"})
 
 
 @app.teardown_appcontext
