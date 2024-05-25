f6324b638347462406a1a3ffe979e4b73babc9ab
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-25 20:02:48 +0300

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,21 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+
+classes = {
+    "Amenity": Amenity,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User,
+}
 
 
 @app_views.route("/status")
@@ -16,4 +31,5 @@ def get_stats():
     """get stats"""
     # add storage.count after merge
     # return jsonify({})
-    return jsonify(storage.count)
+
+    return {k: storage.count(v) for k, v in classes.items()}
