b2a0ced4042382089291df6e42de5adbbf3cc6b6
Salsabil <salsabeelahmed323@gmail.com>
2024-05-26 22:14:48 +0300

    13. Place - Amenity -3

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -17,40 +17,40 @@ def get_place_amenities(place_id):
     return jsonify(amenities)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE'])
-def delete_place_amenity(place_id, amenity_id):
-    """Delete a amenities object"""
-    place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
+# @app_views.route('/places/<place_id>/amenities/<amenity_id>',
+#                  methods=['DELETE'])
+# def delete_place_amenity(place_id, amenity_id):
+#     """Delete a amenities object"""
+#     place = storage.get(Place, place_id)
+#     if place is None:
+#         abort(404)
 
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
-        abort(404)
+#     amenity = storage.get(Amenity, amenity_id)
+#     if amenity is None:
+#         abort(404)
 
-    if amenity in place.amenities is None:
-        abort(404)
+#     if amenity in place.amenities is None:
+#         abort(404)
 
-    place.amenities.remove(amenity)
-    storage.save()
-    return jsonify({}), 200
+#     place.amenities.remove(amenity)
+#     storage.save()
+#     return jsonify({}), 200
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
-def link_amenity_to_place(place_id, amenity_id):
-    """link amenity to place"""
-    place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
+# @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
+# def link_amenity_to_place(place_id, amenity_id):
+#     """link amenity to place"""
+#     place = storage.get(Place, place_id)
+#     if place is None:
+#         abort(404)
 
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
-        abort(404)
+#     amenity = storage.get(Amenity, amenity_id)
+#     if amenity is None:
+#         abort(404)
 
-    if amenity in place.amenities:
-        return jsonify(amenity.to_dict()), 200
+#     if amenity in place.amenities:
+#         return jsonify(amenity.to_dict()), 200
 
-    place.amenities.append(amenity)
-    storage.save()
-    return jsonify(amenity.to_dict()), 201
+#     place.amenities.append(amenity)
+#     storage.save()
+#     return jsonify(amenity.to_dict()), 201
