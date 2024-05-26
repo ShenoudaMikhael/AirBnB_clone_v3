8f98476103cd824563c1c90145db0734f0d212e6
Salsabil <salsabeelahmed323@gmail.com>
2024-05-26 14:03:27 +0300

    15. Search

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,14 @@
 starts a Flask web application
 """
 from os import getenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
+from models.place import Place
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
 
 app = Flask(__name__)
 
@@ -25,6 +29,38 @@ def not_found(e):
 def close_storage(exception):
     """Closes connection"""
     storage.close()
+
+
+@app_views.route('/places_search', methods=['POST'])
+def search_places():
+    """search places"""
+
+    if request.is_json is None:
+        abort(400, jsonify({"error": "Not a JSON"}))
+
+    search_criteria = request.get_json()
+
+    if not search_criteria or all(len(v) == 0 for v in search_criteria.values()):
+        places = storage.all(Place).values()
+        return jsonify([place.to_dict() for place in places])
+
+    places = []
+
+    states = search_criteria.get('states', [])
+    for state_id in states:
+        state = storage.get(State, state_id)
+        if state:
+            places.extend(state.places)
+
+    cities = search_criteria.get('cities', [])
+    for city_id in cities:
+        city = storage.get(City, city_id)
+        if city:
+            places.extend(city.places)
+
+    places = list(set(places))
+
+    return jsonify([place.to_dict() for place in places])
 
 
 if __name__ == "__main__":
