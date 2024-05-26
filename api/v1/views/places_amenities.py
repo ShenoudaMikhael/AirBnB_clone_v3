9fdbe0db86a12bdc583ff09a3ea9fbeda381851f
Salsabil <salsabeelahmed323@gmail.com>
2024-05-26 22:15:37 +0300

    13. Place - Amenity -5

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -37,20 +37,20 @@ def delete_place_amenity(place_id, amenity_id):
     return jsonify({}), 200
 
 
-# @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
-# def link_amenity_to_place(place_id, amenity_id):
-#     """link amenity to place"""
-#     place = storage.get(Place, place_id)
-#     if place is None:
-#         abort(404)
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
+def link_amenity_to_place(place_id, amenity_id):
+    """link amenity to place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
 
-#     amenity = storage.get(Amenity, amenity_id)
-#     if amenity is None:
-#         abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
 
-#     if amenity in place.amenities:
-#         return jsonify(amenity.to_dict()), 200
+    if amenity in place.amenities:
+        return jsonify(amenity.to_dict()), 200
 
-#     place.amenities.append(amenity)
-#     storage.save()
-#     return jsonify(amenity.to_dict()), 201
+    place.amenities.append(amenity)
+    storage.save()
+    return jsonify(amenity.to_dict()), 201
