403c395a61deccac9980d7cf7ea4d72cde61951e
Salsabil <salsabeelahmed323@gmail.com>
2024-05-26 22:19:18 +0300

    places_reviews.py edit

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""create a file review.py"""
+"""create routes for places_reviews.py"""
 from flask import request, abort, jsonify
 from api.v1.views import app_views
 from models import storage
