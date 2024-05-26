1856c96de2637dea60033af8e35e849f5198949d
Salsabil <salsabeelahmed323@gmail.com>
2024-05-26 22:15:16 +0300

    13. Place - Amenity -4

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -17,24 +17,24 @@ def get_place_amenities(place_id):
     return jsonify(amenities)
 
 
-# @app_views.route('/places/<place_id>/amenities/<amenity_id>',
-#                  methods=['DELETE'])
-# def delete_place_amenity(place_id, amenity_id):
-#     """Delete a amenities object"""
-#     place = storage.get(Place, place_id)
-#     if place is None:
-#         abort(404)
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'])
+def delete_place_amenity(place_id, amenity_id):
+    """Delete a amenities object"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
 
-#     amenity = storage.get(Amenity, amenity_id)
-#     if amenity is None:
-#         abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
 
-#     if amenity in place.amenities is None:
-#         abort(404)
+    if amenity in place.amenities is None:
+        abort(404)
 
-#     place.amenities.remove(amenity)
-#     storage.save()
-#     return jsonify({}), 200
+    place.amenities.remove(amenity)
+    storage.save()
+    return jsonify({}), 200
 
 
 # @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
